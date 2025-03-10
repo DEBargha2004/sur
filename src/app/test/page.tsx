79c8732f7ebe58927fedf89a24f7446680601222
DEBargha2004 <debarghasaha16@gmail.com>
2025-03-10 17:56:23 +0000

    feat
    pagination added

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -7,6 +7,10 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import SurParamForm from "@/components/custom/forms/sur-param";
 import { Raag, raag } from "@/constants/raag";
+import { formatNumber } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const MAX_PER_PAGE = 1000;
 
 export default function Page() {
   const [surArray, setSurArray] = useState<string[][]>([]);
@@ -15,6 +19,11 @@ export default function Page() {
     resolver: zodResolver(surParamSchema),
     defaultValues: defaultSurSchema,
   });
+  const [currentPage, setCurrentPage] = useState(0);
+  const currentPageSurs = surArray.slice(
+    currentPage * MAX_PER_PAGE,
+    (currentPage + 1) * MAX_PER_PAGE
+  );
 
   const onSubmit = async (data: TSurSchema) => {
     const sequence = raag[data.raag as Raag].aroha;
@@ -24,18 +33,33 @@ export default function Page() {
       raag: sequence,
     });
     setSurArray(surs);
+    setCurrentPage(0);
   };
+
+  const goTo = (index: number) => {
+    if (index >= 0 && index <= Math.floor(surArray.length / MAX_PER_PAGE)) {
+      setCurrentPage(index);
+    }
+  };
+
+  const [currRangeStart, currRangeEnd] = [
+    Math.min(MAX_PER_PAGE * currentPage + 1, surArray.length),
+    Math.min(MAX_PER_PAGE * (currentPage + 1), surArray.length),
+  ];
   return (
     <main className="p-5 space-y-10">
       <section className="sm:w-1/2 md:w-1/3 xl:w-1/4">
         <SurParamForm form={form} onSubmit={onSubmit} />
       </section>
       <section className="space-y-6">
-        <h1 className="text-xl font-medium">{surArray.length} combinations</h1>
+        <h1 className="text-xl font-medium">
+          {formatNumber(currRangeStart)} - {formatNumber(currRangeEnd)}/
+          {formatNumber(surArray.length)} combinations
+        </h1>
         <ol className="space-y-5">
-          {surArray.map((s, i) => (
+          {currentPageSurs.map((s, i) => (
             <li key={i} className="flex justify-start items-start gap-4">
-              <p>{i + 1}.</p>
+              <p>{MAX_PER_PAGE * currentPage + i + 1}.</p>
               <div className="flex flex-wrap gap-4 gap-y-2">
                 {s.map((sur, j) => (
                   <span
@@ -49,6 +73,10 @@ export default function Page() {
             </li>
           ))}
         </ol>
+        <div className="flex gap-4">
+          <Button onClick={() => goTo(currentPage - 1)}>Prev</Button>
+          <Button onClick={() => goTo(currentPage + 1)}>Next</Button>
+        </div>
       </section>
     </main>
   );
